9ddc1f3939f461415ed90c33080217453c930f55
qori-aziz <muhamadqori@gmail.com>
2023-02-28 07:12:35 +0700

    change debug program

--- test program/firsttest.cpp
+++ test program/firsttest.cpp
@@ -18,7 +18,7 @@ int main()
     Func_ServoStopMotor ServoStopMotor = LoadServoStopMotor(hModule);
     Func_ServoSetGain ServoSetGain = LoadServoSetGain(hModule);
 
-    int numberOfModules = NmcInit("COM1:", 19200);
+    int numberOfModules = NmcInit("COM3:", 19200);
     printf("Number of Modules detected: %d\n", numberOfModules);
     if (numberOfModules == 0){
         printf("failed to initialize. Shutdown.");
@@ -32,7 +32,7 @@ int main()
         printf("OK\n");
     }
 
-    for (int i = 1; i <= 3; i++)
+    for (int i = 1; i <= 2; i ++)
     {
         ServoStopMotor(i, AMP_ENABLE | MOTOR_OFF);   // enable amp
         ServoStopMotor(i, AMP_ENABLE | STOP_ABRUPT); // stop at current pos.
@@ -49,25 +49,96 @@ int main()
                      1,    // SR = 1
                      0     // DC = 0
         );
+    }
+
+    for (int i = 1; i <= 2; i++)
+    {
+        BOOL success = ServoLoadTraj(i, // addr = 2
+                                     LOAD_POS | VEL_MODE| LOAD_VEL | LOAD_ACC | ENABLE_SERVO | START_NOW,
+                                     5000,  // pos = 2000
+                                     100000, // vel = 100,000
+                                     1000,   // acc = 100
+                                     0       // pwm = 0
+        );
+
+        printf("%d\n", success);
+    }
+
+    Sleep(2000);
+
+    for (int i = 1; i <= 2 ; i++)
+    {
+        // ServoStopMotor(i, AMP_ENABLE | MOTOR_OFF);   // enable amp
+        // ServoStopMotor(i, AMP_ENABLE | STOP_ABRUPT); // stop at current pos.
+        // ServoResetPos(i);                            // reset the posiiton counter to 0
+
+        // ServoSetGain(i, // axis = 1
+        //              100,  // Kp = 100
+        //              1000, // Kd = 1000
+        //              0,    // Ki = 0
+        //              0,    // IL = 0
+        //              255,  // OL = 255
+        //              0,    // CL = 0
+        //              4000, // EL = 4000
+        //              1,    // SR = 1
+        //              0     // DC = 0
+        // );
 
         BOOL success = ServoLoadTraj(i, // addr = 2
-                                     LOAD_POS | LOAD_VEL | LOAD_ACC | ENABLE_SERVO | START_NOW,
+                                     LOAD_POS | VEL_MODE|LOAD_VEL | LOAD_ACC | ENABLE_SERVO | START_NOW,
                                      5000,  // pos = 2000
-                                     300000, // vel = 100,000
+                                     0, // vel = 100,000
                                      1000,   // acc = 100
                                      0       // pwm = 0
         );
         printf("%d\n", success);
-
-        byte statbyte;
-        printf("%d\n", statbyte);
-        do
-        {
-            NmcNoOp(i); // poll controller to get current status data
-            statbyte = NmcGetStat(i);
-            printf("%d\n", statbyte);
-        } while (!(statbyte & MOVE_DONE)); // wait for MOVE_DONE bit to go HIGH
     }
+
+    Sleep(2000);
+    
+    for (int i = 1; i <= 2; i++)
+    {
+        BOOL success = ServoLoadTraj(i, // addr = 2
+                                     LOAD_POS | VEL_MODE| LOAD_VEL | LOAD_ACC | ENABLE_SERVO | START_NOW,
+                                     5000,  // pos = 2000
+                                     -100000, // vel = 100,000
+                                     1000,   // acc = 100
+                                     0       // pwm = 0
+        );
+
+        printf("%d\n", success);
+    }
+    Sleep(2000);
+
+     for (int i = 1; i <= 2 ; i++)
+    {
+        // ServoStopMotor(i, AMP_ENABLE | MOTOR_OFF);   // enable amp
+        // ServoStopMotor(i, AMP_ENABLE | STOP_ABRUPT); // stop at current pos.
+        // ServoResetPos(i);                            // reset the posiiton counter to 0
+
+        // ServoSetGain(i, // axis = 1
+        //              100,  // Kp = 100
+        //              1000, // Kd = 1000
+        //              0,    // Ki = 0
+        //              0,    // IL = 0
+        //              255,  // OL = 255
+        //              0,    // CL = 0
+        //              4000, // EL = 4000
+        //              1,    // SR = 1
+        //              0     // DC = 0
+        // );
+
+        BOOL success = ServoLoadTraj(i, // addr = 2
+                                     LOAD_POS | VEL_MODE|LOAD_VEL | LOAD_ACC | ENABLE_SERVO | START_NOW,
+                                     5000,  // pos = 2000
+                                     0, // vel = 100,000
+                                     1000,   // acc = 100
+                                     0       // pwm = 0
+        );
+        printf("%d\n", success);
+    }
+
+
     NmcShutdown();
 
     FreeLibrary(hModule);
